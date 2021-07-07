9b7abca5d3a68a8db074a5a40557d24634103c0a
hamid <hamidq88@gmail.com>
2021-07-08 00:43:03 +0100

    Update main.js
    Updates includes additional functions

--- main.js
+++ main.js
@@ -18,20 +18,16 @@ function pAequorFactory(num, dna_arr) {
     specimenNum: num, 
     dna: dna_arr,
     method (){ //randomly selects a base in objects dna property and changing current base to a different base.
-      console.log(dna_arr);
       let random = Math.floor(Math.random() * 15);
       let newBase = returnRandBase();
       while(dna_arr[random] == newBase) {
         newBase = returnRandBase();
       } 
       dna_arr[random] = newBase
-      console.log(dna_arr);
     },
     compareDNA(pAequor){
-      console.log(this.dna);
-      console.log(pAequor.dna);
       let match = 0;
-      for (let i = 0; i < this.dna.length-1; i++){
+      for (let i = 0; i <= this.dna.length-1; i++){
         if(this.dna[i] == pAequor.dna[i]){
           match += 1;
         }
@@ -39,6 +35,22 @@ function pAequorFactory(num, dna_arr) {
       let ratio = Math.round((match/this.dna.length)*100);
 
       return `specimen #1 and specimen #2 have ${ratio}% DNA in common`;
+    },
+    willLikelySurvive() {
+      let CandGBases  = 0;
+      for (base of this.dna){
+        if(base == 'G' || base == 'C'){
+          CandGBases += 1;
+        }
+      }
+      let ratio = Math.round((CandGBases/this.dna.length)*100);
+      // console.log(ratio);
+      if (ratio >= 60) {
+        return true;
+      } else {
+        return false;
+      }
+
     }
   });
 }
@@ -47,12 +59,23 @@ const test1 = pAequorFactory(1, mockUpStrand());
 const test2 = pAequorFactory(2, mockUpStrand());
 
 // console.log(test.dna);
-console.log(test1.compareDNA(test2));
+// console.log(test1.compareDNA(test2));
 
-// test.method();
+// console.log(test1.willLikelySurvive());
 
+function survivalSamples () {
+  let sample = [];
+  for (let count = 0; count < 30; count++){
+    let tempSample = pAequorFactory(count, mockUpStrand());
+    while(tempSample.willLikelySurvive() == false){
+      tempSample = pAequorFactory(count, mockUpStrand());
+    }
+    // console.log(tempSample.willLikelySurvive());
+    sample.push(tempSample.dna);
+  }
+  return sample;
+}
 
-// console.log(test.dna);
-
+console.log(survivalSamples());
 
 
